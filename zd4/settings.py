8a6088f44014f219ff30fe3bf970f972275a5f1d
DenisRudakov0 <79634508+DenisRudakov0@users.noreply.github.com>
2021-11-07 14:27:28 +0300

    create new app

--- zd4/settings.py
+++ zd4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b6oky$r@7&te(#0jn56gjt-j5byk5upr8ap@h31@q5*#ft9#05
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
